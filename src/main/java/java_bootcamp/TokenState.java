7dbc06e3bd8d38af12f20d9b19fb0bbd37332ddf
atilcock <a_tilcock@hotmail.com>
2019-04-01 10:37:01 -0700

    test

--- src/main/java/java_bootcamp/TokenState.java
+++ src/main/java/java_bootcamp/TokenState.java
@@ -4,11 +4,59 @@ import com.google.common.collect.ImmutableList;
 import net.corda.core.contracts.ContractState;
 import net.corda.core.identity.AbstractParty;
 import net.corda.core.identity.Party;
+import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /* Our state, defining a shared fact on the ledger.
  * See src/main/java/examples/ArtState.java for an example. */
-public class TokenState {
+public class TokenState implements ContractState{
 
+    private Party issuer;
+
+    public Party getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Party owner) {
+        this.owner = owner;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    private Party owner;
+    private int amount;
+
+    public TokenState(Party issuer, Party owner, int amount)
+    {
+        this.issuer=issuer;
+        this.owner=owner;
+        this.amount=amount;
+
+    }
+
+
+    @NotNull
+    @Override
+    public List<AbstractParty> getParticipants() {
+        List<AbstractParty> participants = new ArrayList<>();
+        participants.add(issuer);
+        participants.add(owner);
+        return participants;
+    }
+
+    public Party getIssuer() {
+        return issuer;
+    }
+
+    public void setIssuer(Party issuer) {
+        this.issuer = issuer;
+    }
 }
